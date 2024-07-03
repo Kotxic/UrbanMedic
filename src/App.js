dae349ea551421b07c405f8ecce7a7d65ea79b87
Kotxic <nesterov.d.c@mail.ru>
2024-07-03 18:07:06 +0400

    Test Version 1.1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
-import Login from "./Pages/Login";
-import Main from "./Pages/Main";
-import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
+import React from 'react';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './Pages/Login';
+import Main from './Pages/Main';
+import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute';
 function App() {
-  return (
-      <BrowserRouter>
-        <Routes>
-            <Route path={'/Login'} element={<Login/>}/>
-            <Route path={'/Main'} element={<ProtectedRoute> <Main/></ProtectedRoute>}/>
-            <Route path="*" element={<Navigate to='/Login' replace/>}/>
-        </Routes>
-      </BrowserRouter>
-
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/Login" element={<Login />} />
+                <Route path="/Main" element={<ProtectedRoute><Main /></ProtectedRoute>} />
+                <Route path="*" element={<Navigate to="/Login" replace />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
